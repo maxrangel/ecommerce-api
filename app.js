0f73caedf690a8cd03851f88faa74b89f0a30e4e
maxrangel <lik4ever@hotmail.com>
2022-01-31 21:24:08 -0600

    ready for deployment#

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const path = require('path');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
+
+// Routers
+const { userRouter } = require('./routes/users.routes');
+const { productsRouter } = require('./routes/products.routes');
+const { ordersRouter } = require('./routes/orders.routes');
+const { viewsRouter } = require('./routes/views.routes');
+
+// Controllers
+const { globalErrorHandler } = require('./controllers/error.controller');
+
+// Utils
+const { AppError } = require('./utils/appError');
+
+// Init app
+const app = express();
+
+app.enable('trust proxy');
+
+app.use(xss());
+app.use(helmet());
+app.use(compression());
+
+app.use(
+	rateLimit({
+		max: 1000,
+		windowMs: 60 * 60 * 1000, // 1 hour
+		message: 'Too many requests from this IP',
+	})
+);
+
+app.use(express.static(path.join(__dirname, 'views')));
+app.set('view-engine', 'pug');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Implement CORS
+app.use(cors()); //Access-Control-Allow-Origin *
+app.options('*', cors());
+
+app.use(cookieParser());
+
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+else app.use(morgan('combined'));
+
+// Endpoints
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/products', productsRouter);
+app.use('/api/v1/orders', ordersRouter);
+app.use('/', viewsRouter);
+
+app.use('*', (req, res, next) => {
+	next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = { app };
---
+++ controllers/orders.controller.js
@@ -0,0 +1,219 @@
+// Models
+const { Product } = require('../models/product.model');
+const { Cart } = require('../models/cart.model');
+const { ProductInCart } = require('../models/productInCart.model');
+
+// Utils
+const { catchAsync } = require('../utils/catchAsync');
+const { filterObj } = require('../utils/filterObj');
+const { AppError } = require('../utils/appError');
+const { formatUserCart } = require('../utils/queryFormat');
+
+exports.getUserCart = catchAsync(async (req, res, next) => {
+	const { currentUser } = req;
+
+	const cart = await Cart.findOne({
+		attributes: { exclude: ['userId', 'status'] },
+		where: { userId: currentUser.id, status: 'onGoing' },
+		include: [
+			{
+				model: ProductInCart,
+				attributes: { exclude: ['cartId', 'status'] },
+				where: { status: 'active' },
+				include: [
+					{
+						model: Product,
+						attributes: {
+							exclude: ['id', 'userId', 'price', 'quantity', 'status'],
+						},
+					},
+				],
+			},
+		],
+	});
+
+	const formattedCart = formatUserCart(cart);
+
+	res.status(200).json({
+		status: 'success',
+		data: { cart: formattedCart },
+	});
+});
+
+exports.addProductToCart = catchAsync(async (req, res, next) => {
+	const { product } = req.body;
+	const { currentUser } = req;
+
+	const filteredObj = filterObj(product, 'id', 'quantity');
+
+	// Validate if quantity is less or equal to existing quantity
+	const productExists = await Product.findOne({
+		where: { id: filteredObj.id, status: 'active' },
+	});
+
+	if (!productExists || filteredObj.quantity > productExists.quantity) {
+		return next(
+			new AppError(
+				'Product does not exists or it exceeds the available quantity',
+				400
+			)
+		);
+	}
+
+	// Check if current user already has a cart
+	const cart = await Cart.findOne({
+		where: { userId: currentUser.id, status: 'onGoing' },
+	});
+
+	// Create new cart
+	if (!cart) {
+		const totalPrice = +filteredObj.quantity * +productExists.price;
+
+		const newCart = await Cart.create({ userId: currentUser.id, totalPrice });
+
+		await ProductInCart.create({
+			cartId: newCart.id,
+			productId: filteredObj.id,
+			quantity: filteredObj.quantity,
+			price: productExists.price,
+		});
+	}
+
+	// Update cart
+	if (cart) {
+		// Check if product already exists on the cart
+		const productInCartExists = await ProductInCart.findOne({
+			where: {
+				cartId: cart.id,
+				productId: filteredObj.id,
+				status: 'active',
+			},
+		});
+
+		if (productInCartExists) {
+			return next(
+				new AppError('You already added this product to the cart', 400)
+			);
+		}
+
+		// Add it to the cart
+		await ProductInCart.create({
+			cartId: cart.id,
+			productId: filteredObj.id,
+			quantity: filteredObj.quantity,
+			price: productExists.price,
+		});
+
+		// Calculate the cart total price
+		const updatedTotalPrice =
+			+cart.totalPrice + +filteredObj.quantity * +productExists.price;
+
+		await cart.update({ totalPrice: updatedTotalPrice });
+	}
+
+	res.status(201).json({ status: 'success' });
+});
+
+exports.updateProductCart = catchAsync(async (req, res, next) => {
+	const { currentUser } = req;
+	const { productId, newQuantity } = req.body;
+
+	// Find user's cart
+	const userCart = await Cart.findOne({
+		where: { userId: currentUser.id, status: 'onGoing' },
+	});
+
+	if (!userCart) {
+		return next(new AppError('Invalid cart', 400));
+	}
+
+	// Find product in cart
+	const productInCart = await ProductInCart.findOne({
+		where: {
+			productId,
+			cartId: userCart.id,
+			status: 'active',
+		},
+		include: [{ model: Product }],
+	});
+
+	if (!productInCart) {
+		return next(new AppError('Invalid product', 400));
+	}
+
+	if (newQuantity > +productInCart.product.quantity) {
+		return next(
+			new AppError(
+				`This product only has ${productInCart.product.quantity} items`,
+				400
+			)
+		);
+	}
+
+	if (newQuantity === productInCart.quantity) {
+		return next(
+			new AppError('You already have that quantity in that product', 400)
+		);
+	}
+
+	let updatedTotalPrice;
+
+	// Check if user added or removed from the selected product
+	// If user send 0 quantity to product, remove it from the cart
+	if (newQuantity === 0) {
+		updatedTotalPrice =
+			+userCart.totalPrice - +productInCart.quantity * +productInCart.price;
+
+		// Update quantity to product in cart
+		await productInCart.update({ quantity: 0, status: 'removed' });
+	} else if (newQuantity > +productInCart.quantity) {
+		// New items were added
+		updatedTotalPrice =
+			+userCart.totalPrice +
+			(newQuantity - +productInCart.quantity) * +productInCart.price;
+
+		// Update quantity to product in cart
+		await productInCart.update({ quantity: newQuantity });
+	} else if (newQuantity < +productInCart.quantity) {
+		// Items were removed from the cart
+		updatedTotalPrice =
+			+userCart.totalPrice -
+			(+productInCart.quantity - newQuantity) * +productInCart.price;
+
+		// Update quantity to product in cart
+		await productInCart.update({ quantity: newQuantity });
+	}
+
+	// Calculate new total price
+	await userCart.update({ totalPrice: updatedTotalPrice });
+
+	res.status(204).json({ status: 'success' });
+});
+
+exports.purchaseOrder = catchAsync(async (req, res, next) => {
+	// 1st part:
+	// Get user's cart and get the products of the cart
+	// Set Cart status to 'purchased'
+	// Create a new order
+
+	// [Promise]
+	// const promises = products.map(async () => {
+	// 	await Product.findAll()
+	// })
+
+	// await Promise.all(promises)
+
+	// Loop through the products array, for each product
+	// Set productInCart status to 'purchased', search for cartId and productId
+	// Look for the Product (productId), substract and update the requested qty from the product's qty
+	// Create productInOrder, pass orderId, productId, qty, price
+
+	// 2nd part:
+	// Send email to the user that purchased the order
+	// The email must contain the total price and the list of products that it purchased
+
+	res.status(200).json({ status: 'success' });
+});
+
+// Create a controller a function that gets all the user's orders
+// The response must include all products that purchased
